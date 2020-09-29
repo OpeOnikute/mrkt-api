0198dfa17dae9c8c7f8b64e18ecf5a5e8a521d27
Opeyemi <opeyemionikute@yahoo.com>
2020-09-29 17:40:16 +0100

    allow CORS

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 
 	"github.com/OpeOnikute/mrkt-api/db"
 	"github.com/OpeOnikute/mrkt-api/router"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
 	db.Connect()
 	fmt.Printf("Application listening on port %s\n", PORT)
-	http.ListenAndServe(fmt.Sprintf(":%s", PORT), router.GetRouter())
+
+	// handle CORS requests
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	r := router.GetRouter()
+
+	http.ListenAndServe(fmt.Sprintf(":%s", PORT), handlers.CORS(originsOk, headersOk, methodsOk)(r))
 }
